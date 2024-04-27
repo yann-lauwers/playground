ccfb0d507d229bdf4a3766b65c9f6d4c65b3f9c9
Lauwers Yann <76515349+yann-embie@users.noreply.github.com>
2024-04-27 11:30:14 +0200

    refactor: remove project manager and replace fullstack by frontend in header main page

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -270,12 +270,12 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Full stack developer and junior project manager
+            Front End web developer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I&apos;m Yann, a full stack web developer and junior project manager
-            based in Brussels. I develop experiences users love with a great
-            focus on the product itself.
+            I&apos;m Yann, a front end web developer based in Brussels. I
+            develop experiences users love with a great focus on the product
+            itself.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
